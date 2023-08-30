5c8af066b9506f1dfdb30d583519958834ece6cb
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-30 14:44:00 +0100

    App.js: routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import Header from './components/header.js';
-import Footer from './components/footer.js';
-import Container from 'react-bootstrap/Container';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/Layout.js';
+import Home from './pages/Home.js';
+import FacsimileView from './pages/FacsimileView.js';
+import TranscriptionView from "./pages/TranscriptionView.js";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Container fluid style={{ verticalAlign: "center", textAlign: "center", height: "85vh" }}>
-        <h1 style={{ paddingTop: '2.0rem' }}>The Digital 'Anon'</h1>
-        <h2>A Digital, Genetic Edition of Virginia Woolf's Final Essays</h2>
-        <br />
-        <p style={{ fontSize: '10.0rem' }}>🚧</p>
-        <p style={{ fontSize: '1.5rem', fontWeight: 'bolder' }}>This site is under construction. Please check back later.</p>
-      </Container>
-      <Footer />
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/facsimiles" element={<FacsimileView />} />
+            <Route path="/transcriptions:transcriptionId" element={<TranscriptionView />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
