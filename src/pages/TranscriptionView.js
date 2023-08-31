1f9fe949c7d62e60fdc6c265f2d1a4ee9f9ed5b9
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-31 16:04:27 +0100

    mucking around with CSS

--- src/pages/TranscriptionView.js
+++ src/pages/TranscriptionView.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Construction from '../components/Construction';
+import Subheader from '../components/transcriptions/Subheader';
 
 const TranscriptionView = ({ page }) => {
 
   return (
     <>
+      <Subheader />
       <Construction page={"Transcriptions"} />
     </>
   )
