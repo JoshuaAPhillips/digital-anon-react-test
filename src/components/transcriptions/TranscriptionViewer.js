ee4534ad1767dcf4154253c1f6e00d2c02f17918
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-09-02 23:53:46 +0100

    Very rough prototype of transcription viewer up and running

--- src/components/transcriptions/TranscriptionViewer.js
+++ src/components/transcriptions/TranscriptionViewer.js
@@ -9,30 +9,44 @@ const TranscriptionViewer = ({ documentData }) => {
   const matchingPage = documentData.find((document) => document.id === page_id);
   const matchingPageUri = matchingPage ? matchingPage.transcription_html_uri : null;
   const [pageContent, setPageContent] = useState(null);
+  const [error, setError] = useState(false);
+
 
   useEffect(() => {
     if (matchingPage && matchingPageUri) {
       fetch(matchingPageUri)
-        .then(response => response.text())
-        .then(html => setPageContent(html));
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Failed to fetch');
+          }
+          return response.text();
+        })
+        .then(html => {
+          setPageContent(html);
+          setError(false); // Reset error flag if fetch is successful
+        })
+        .catch(error => {
+          setError(true); // Set error flag if an error occurs
+        });
     } else {
-      setPageContent(<Construction page={"Transcriptions - " + page_id} />);
+      setError(false); // Reset error flag if there's no matching page or URI
     }
-
   }, [page_id, matchingPage, matchingPageUri]);
 
-  if (!page_id) {
-    return <Construction page={"Transcriptions"} />
+  if (!matchingPageUri) {
+    return <Construction page={"Transcriptions - " + page_id} />;
   } else if (!matchingPage) {
-    return <NotFoundError />
+    return <NotFoundError />;
+  } else if (error) {
+    return <NotFoundError />; // Render error component if an error occurred during fetch
   }
 
   return (
     <>
       <p>{matchingPage.title_full}</p>
-      {pageContent}
+      <div dangerouslySetInnerHTML={{ __html: pageContent }}></div>
     </>
   );
 };
 
-export default TranscriptionViewer
+export default TranscriptionViewer;
