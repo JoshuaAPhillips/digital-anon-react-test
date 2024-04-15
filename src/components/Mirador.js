2757650b3b1aa2f2425f975b4f3ce3a6e82a1486
Joshua Phillips <joshua.phillips@ell.ox.ac.uk>
2024-04-15 21:16:25 +0100

    some more work on Mirador, unsuccessful

--- src/components/Mirador.js
+++ src/components/Mirador.js
@@ -1,24 +1,10 @@
 import React, { Component } from "react";
 import mirador from "mirador";
-
-const loadedManifests = [
-  {
-    manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-1-manifest.json",
-    provider: "NYPL"
-  }
-]
+import { id } from "postcss-selector-parser";
 
 class Mirador extends Component {
   componentDidMount() {
-    const config = {
-      id: "mirador",
-      catalog: [
-        {
-          loadedManifest: loadedManifests.manifestId
-        }
-      ]
-    }
-    const { plugins } = this.props;
+    const { config, plugins } = this.props;
     mirador.viewer(config, plugins);
   }
 
--- src/pages/FacsimileView.js
+++ src/pages/FacsimileView.js
@@ -4,18 +4,142 @@ import { useParams } from 'react-router-dom';
 
 const FacsimileView = ({ page }) => {
 
+  const manifests = [
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-1-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-2-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-3-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-4-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-5-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-6-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-7-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-8-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-9-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m1-8-10-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m45-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m46-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m47-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m48-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m49-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m50-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m51-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m52-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m53-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-1-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-2-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-3-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-4-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-5-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-6-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-7-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-8-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-9-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-10-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m54-11-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m108-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m109-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m110-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m111-1-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m111-2-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m111-3-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m112-1-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m112-2-manifest.json",
+    },
+    {
+      manifestId: "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/manifests/m113-3-manifest.json",
+    },
+  ]
+  
+  const config = {
+    id: "mirador",
+    catalog: manifests
+  }
+
   return(
     <>
-    <div style={{position: 'relative', width: '100%', height: '85vh', border: 'none'}}>
-      <Mirador config={{ 
-        id: "mirador"
-        }} plugins={[]} />
-    </div>
+    {/*<div style={{position: 'relative', width: '100%', height: '85vh', border: 'none'}}>
+      <Mirador config={config}
+        plugins={[]} />
+  </div>*/}
       
-      {/*<iframe 
+      <iframe 
         src="https://joshuaaphillips.github.io/digital-anon/html/miradortest.html" 
         style={{width: '100%', height: '85vh', border: 'none'}}
-  />*/}
+  />
     </>
   )
 }
